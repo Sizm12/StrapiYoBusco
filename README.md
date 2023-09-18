9f1e1f0d94d0a258f445087013437052225d6f30
Sizm12 <s.zeledon.cbe@gmail.com>
2023-09-18 11:59:31 -0600

    Push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# StrapiYoBusco
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'abb062c8-8222-4362-aeca-43dd7fc3499f'
+};
